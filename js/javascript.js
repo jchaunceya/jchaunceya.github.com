68e3eaf1bf8cfabfc748c6aaba048d1fdf6135af
jchaunceya <jchaunceya@gmail.com>
2016-07-21 21:19:06 -0700

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+     checkSize();
+     $(window).resize(checkSize);
+});
+
+function checkSize() {
+     if ($(window).width() <= 543)
+     {
+          $("#collapsing-content").collapse("hide");
+          $("#img-box").hide();
+          $("#navigation-column").prependTo("#info-segment");
+          $("#li-target").removeClass("padding-top-target");
+     }
+     else
+     {
+          $("#collapsing-content").collapse("show");
+          $("#img-box").show();
+          $("#navigation-column").appendTo("#info-segment");
+          $("#li-target").addClass("padding-top-target");
+     }
+}
