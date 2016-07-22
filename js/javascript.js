7e4996bbd56a0be03d75c274493a6f8c6ab6baa8
jchaunceya <jchaunceya@gmail.com>
2016-07-21 21:43:53 -0700

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -6,14 +6,14 @@ $(document).ready(function() {
 function checkSize() {
      if ($(window).width() <= 543)
      {
-          $("#collapsing-content").collapse("hide");
+          // $("#collapsing-content").collapse("hide");
           $("#img-box").hide();
           $("#navigation-column").prependTo("#info-segment");
           $("#li-target").removeClass("padding-top-target");
      }
      else
      {
-          $("#collapsing-content").collapse("show");
+          // $("#collapsing-content").collapse("show");
           $("#img-box").show();
           $("#navigation-column").appendTo("#info-segment");
           $("#li-target").addClass("padding-top-target");
