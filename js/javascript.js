8ec6d9ef9ba07534c819a0fc6550257951d57f4d
jchaunceya <jchaunceya@gmail.com>
2016-07-25 14:50:00 -0700

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,36 +1,89 @@
-
-
-
 $(document).ready(function() {
-     checkSize();
-     $(window).resize(checkSize);
+     $(".card").removeClass("darken");
+     $width = widthDeterminer();
+     doSomething($width);
+     $(window).resize(function() {
+          heightSetter();
+          var $newWidth = widthDeterminer();
+          if ($newWidth != $width)
+          {
+               doSomething($newWidth);
+               $width = $newWidth;
+          }
+          else
+               return;
+     });
 });
 
 
-
-function checkSize() {
-     if ($(document).width() <= 543) // hide collapse show button
+function doSomething($size) {
+     if ($size == "xs")
      {
-          $(".large-lg").removeClass("large-lg-css");            // toggles larger font sizes OFF
-          $(".small-lg").removeClass("small-lg-css");
-          $("#navigation-column").css("height", "auto");         // resets nav-height to
+          makeFontSmall();
           $("#img-box").hide();
-          $("#navigation-column").prependTo("#info-segment");
           $("#li-target").removeClass("padding-top-target");
+          $("#navigation-column").appendTo("#jumbo");
+
+          $("#toggle-button").on("click", function() {
+               $("#collapsing-content").on("shown.bs.collapse", function() {
+                    if (widthDeterminer() == "xs")
+                         $("#jumbo-row").slideUp("fast");
+               });
+               $("#collapsing-content").on("hidden.bs.collapse", function() {
+                    $("#jumbo-row").slideDown("fast");
+               });
+          });
+          $(".text-link").css("height", "44px");
      }
      else
      {
-          if ($(document).width() >= 768)
-               $("#img-box").show();
-          else
+          if ($size == "sm")
+          {
                $("#img-box").hide();
-
-          $(".large-lg").addClass("large-lg-css");               // toggles larger font sizes ON
-          $(".small-lg").addClass("small-lg-css");
+          }
+          else
+          {
+               $("#img-box:hidden").show();
+               makeFontLarge();
+          }
+          $("#collapsing-content:hidden").collapse("show");
+          $("#jumbo:hidden").slideDown();
           $("#li-target").addClass("padding-top-target");
-          var $heightOfInfo = $("#info-segment").height();
-          $("#navigation-column").css("height", $heightOfInfo);
-          $("#collapsing-content").collapse("show");
           $("#navigation-column").appendTo("#info-segment");
      }
-}
+     heightSetter();
+
+};
+
+function heightSetter() {
+     var $size = widthDeterminer();
+     if ($size == "xs")
+     {
+          $("#navigation-column").css("height", "auto");         // resets nav-height to
+     }
+     else {
+          var $heightOfInfo = $("#info-box").height();
+          $("#navigation-column").css("height", $heightOfInfo);
+
+     }
+};
+
+function widthDeterminer() {
+     var $curWidth = $(document).width();
+     if ($curWidth <= 543)
+          return "xs";
+     if ($curWidth < 768)
+          return "sm";
+     else
+          return "md";
+};
+
+function makeFontLarge() {
+     $(".large-lg").addClass("large-lg-css");               // toggles larger font sizes ON
+     $(".small-lg").addClass("small-lg-css");
+};
+
+function makeFontSmall() {
+     $(".large-lg").removeClass("large-lg-css");            // toggles larger font sizes OFF
+     $(".small-lg").removeClass("small-lg-css");
+};
